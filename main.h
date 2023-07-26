1517b4f458d575b19c9a7c77cbc283b95efd0412
Yulaco <cleochen93@gmail.com>
2023-07-26 05:05:40 -0700

    Printf header

--- main.h
+++ main.h
@@ -1,5 +1,5 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef HEADER_H
+#define HEADER_H
 
 #include <limits.h>
 #include <stdarg.h>
@@ -112,6 +112,5 @@ unsigned int convert_sbase(buffer_t *output, long int num, char *base,
 		unsigned char flags, int wid, int prec);
 unsigned int convert_ubase(buffer_t *output, unsigned long int num, char *base,
 		unsigned char flags, int wid, int prec);
-
 
 #endif
